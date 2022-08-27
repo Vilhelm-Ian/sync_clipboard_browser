6346ccf322121c0d14d2d883a8084188bcd01d41
Jovan <mariothrowsfieball@gmail.com>
2022-08-27 21:53:10 +0200

    when the user visits the / route the socket connects

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+  res.sendFile(__dirname + "/index.html");
 });
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 fs.watchFile("./clipboard.txt", (curr, prev) => {
