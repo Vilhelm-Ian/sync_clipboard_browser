dd94bf93c1361c048fe81ee8ff7f7f8d1faf448d
Jovan <mariothrowsfieball@gmail.com>
2022-08-27 21:44:51 +0200

    added express server

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const fs = require("fs");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
 
 fs.watchFile("./clipboard.txt", (curr, prev) => {
   console.log(` file Changed`);
